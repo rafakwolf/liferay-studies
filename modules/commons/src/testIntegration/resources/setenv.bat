fc0b79f6c4af523ae00edfbcd7ccddf9d342bb7c
Rafael <rafakwolf@gmail.com>
2018-06-18 17:23:20 -0300

    integration tests (WIP)

--- modules/commons/src/testIntegration/resources/setenv.bat
+++
@@ -1,3 +0,0 @@
-set "JMX_OPTS=-Dcom.sun.management.jmxremote -Dcom.sun.management.jmxremote.authenticate=false -Dcom.sun.management.jmxremote.port=8099 -Dcom.sun.management.jmxremote.ssl=false"
-
-set "CATALINA_OPTS=%CATALINA_OPTS% %JMX_OPTS%"
