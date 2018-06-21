db5b02a0ff65406cd69684249023937e664290c4
Rafael <rafakwolf@gmail.com>
2018-06-21 17:43:23 -0300

    sample rest api

--- modules/myRestApi/src/main/java/myrestapi/application/MyRestApiApplication.java
+++ modules/myRestApi/src/main/java/myrestapi/application/MyRestApiApplication.java
@@ -17,48 +17,48 @@ import java.util.Set;
 @Component(immediate = true, service = Application.class)
 public class MyRestApiApplication extends Application {
 
-	public Set<Object> getSingletons() {
-		return Collections.singleton(this);
-	}
+    public Set<Object> getSingletons() {
+        return Collections.singleton(this);
+    }
 
-	@GET
-	@Produces("text/plain")
-	public String working() {
-		return "It works!";
-	}
+    @GET
+    @Produces("text/plain")
+    public String working() {
+        return "It works!";
+    }
 
-	@GET
-	@Path("/morning")
-	@Produces("text/plain")
-	public String hello() {
-		return "Good morning!";
-	}
+    @GET
+    @Path("/morning")
+    @Produces("text/plain")
+    public String hello() {
+        return "Good morning!";
+    }
 
-	@GET
-	@Path("/morning/{name}")
-	@Produces("text/plain")
-	public String morning(
-		@PathParam("name") String name,
-		@QueryParam("drink") String drink) {
+    @GET
+    @Path("/morning/{name}")
+    @Produces("text/plain")
+    public String morning(
+            @PathParam("name") String name,
+            @QueryParam("drink") String drink) {
 
-		String greeting = "Good Morning " + name;
+        String greeting = "Good Morning " + name;
 
-		if (drink != null) {
-			greeting += ". Would you like some " + drink + "?";
-		}
+        if (drink != null) {
+            greeting += ". Would you like some " + drink + "?";
+        }
 
-		return greeting;
-	}
+        return greeting;
+    }
 
-	@POST
-	@Path("/json-test")
-	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_JSON)
-	public TestEntity jsonTest(TestEntity entity) {
+    @POST
+    @Path("/json-test")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public TestEntity jsonTest(TestEntity entity) {
 
-		entity.setName(entity.getName()+ "- rest api");
+        entity.setName(entity.getName() + "- rest api");
 
-		return entity;
-	}
+        return entity;
+    }
 
 }
